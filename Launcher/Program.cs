452c488f8113384261258da4121937767cfc4029
Aran Kieskamp <aran@arankieskamp.me>
2019-09-19 17:43:58 +0200

    Added Avalonia MVVM

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using Launcher.ViewModels;
+using Launcher.Views;
 
 namespace Launcher
 {
     class Program
     {
-        static void Main(string[] args)
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        public static void Main(string[] args) => BuildAvaloniaApp().Start(AppMain, args);
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToDebug()
+                .UseReactiveUI();
+
+        // Your application's entry point. Here you can initialize your MVVM framework, DI
+        // container, etc.
+        private static void AppMain(Application app, string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var window = new MainWindow
+            {
+                DataContext = new MainWindowViewModel(),
+            };
+
+            app.Run(window);
         }
     }
 }
